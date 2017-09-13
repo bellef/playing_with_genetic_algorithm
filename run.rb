81fcfacc33d26b2c54db0eb7b1afdbd081f36ec1
francois <francois@tikee.com>
2017-09-13 14:09:46 +0200

    Basic fitness function

--- run.rb
+++ run.rb
@@ -1,36 +1,54 @@
 #!/usr/bin/ruby
 # encoding: utf-8
 
-SEED_POPULATION_SIZE = 20
-ALPHABET = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 ? , ; . : / ! § ù % * µ $ £ ^ ¨ = + ( ) ' & é ~ # " { } ° ç @ | `] << " "
+SEED_POPULATION_SIZE = 2
+ALPHABET = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 ? , ; . : / ! § ù % * µ $ £ ^ ¨ = + ( ) ' & é ~ # " { } ° ç @ | `] << ' '
 
-
-def fitness(passphrase)
-  # TODO
+# Gives a score to every member of the population
+def fitness(member)
+  member[:score] = 0
+  member[:phrase].each_char.with_index do |member_char, i|
+    member[:score] += 10 if member_char == @passphrase[i] # Char is at the right place
+    member[:score] += 5  if (member_char != @passphrase[i]) && @passphrase.include?(member_char) # Char is at the wrong place
+  end
 end
 
 # Create the initial population
-def seed(passphrase)
-  length = passphrase.length
-  seed_population = []
+def seed
+  length = @passphrase.length
 
   SEED_POPULATION_SIZE.times do |i|
-    seed_population[i] = ALPHABET.shuffle[0..length].join
+    @seed_population[i] = {
+      phrase: ALPHABET.shuffle[0..length].join, # IMPROVE
+      score: 0
+    }
   end
-  seed_population
+end
+
+def evolution(population_n)
+  population_n.each do |member|
+    fitness member
+    puts member
+  end
 end
 
 def main
+  @problem_solved = false
+  @seed_population = []
+
   puts "Please enter a passphrase that the algorithm will crack (20 char max):"
-  passphrase = gets.chomp[0..19]
-  puts "\nPassphrase to find: \n[#{passphrase}]"
+  @passphrase = gets.chomp[0..19]
+  puts "\nPassphrase to find: \n[#{@passphrase}]"
 
-  puts "STEP 1: Create a seed population"
-  population = seed(passphrase)
-  puts "Population intiated:"
-  puts population
+  puts '------------------------------------------'
+  puts 'STEP 1: Create a seed population'
+  seed
+  puts 'Population intiated:'
+  puts @seed_population.map { |el| el[:phrase] }
 
-  fitness(passphrase)
+  puts '------------------------------------------'
+  puts 'STEP 2: Start a loop to find the passphrase'
+  evolution(@seed_population)
 end
 
 main
